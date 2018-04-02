fdb40466e70fa8e83d83d198ae0fb273318e7481
Apollo <jackson.harris3@gmail.com>
2018-04-02 02:28:37 -0700

    begin portfolio, add section animations

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,43 +17,35 @@ $(document).ready( function(){
     // document.getElementById('code').innerHTML = msg;
     document.body.style.background = url;
 
-    // anime({
-    //     targets: 'path',
-    //     translateX: [
-    //         { value: 100, duration: 1200 },
-    //         { value: 0, duration: 800 }
-    //     ],
-    //     rotate: '1turn',
-    //     backgroundColor: '#FFF',
-    //     duration: 2000,
-    //     loop: true
-    // });
+    //initialize scrollfire
+    var options = [
+        {
+            selector: '#home', offset: 50, callback: function (el) {
+                Materialize.toast("Welcome to my Portfolio!", 4500);
+            }
+        },
+        {
+            selector: '#portfolio-items', offset: 400, callback: function (el) {
+                Materialize.showStaggeredList($(el));
+            }
+        },
+    ];
 
-    var easing = anime({
-        targets: '#easing .el',
-        translateX: 250,
-        easing: 'easeInOutQuart'
-    });
+    Materialize.scrollFire(options);
 
-    anime({
-        targets: '#svgAttributes path',
-        points: 'M28 0L28 34L0 50L0 84L28 100L56 84L56 50L28 34'
-    });
-
-    // Initialize collapse button
+    //initialize side nav
     $(".button-collapse").sideNav();
-  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  //$('.collapsible').collapsible();
 
-    $(".dropdown-trigger").dropdown();
-    // $('.fixed-action-btn').floatingActionButton(open())
-
-    // <svg xmlns='http://www.w3.org/2000/svg' width='56' height='100'>
-    //     <rect width='56' height='100' fill='#000000' />
-    //     <path d='M28 66L0 50L0 16L28 0L56 16L56 50L28 66L28 100' fill='none' stroke='#fff629' stroke-width='2' />
-    //     <path d='M28 0L28 34L0 50L0 84L28 100L56 84L56 50L28 34' fill='none' stroke='#ffe503' stroke-width='2' />
-    // </svg>
-
+    //initialize slider
+    $('.slider').slider();
+    // Pause slider
+    $('.slider').slider('pause');
+    // Start slider
+    $('.slider').slider('start');
+    // Next slide
+    $('.slider').slider('next');
+    // Previous slide
+    $('.slider').slider('prev');
 
 
 })
